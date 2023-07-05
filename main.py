6627b8d6ed68c25259ab2a53c04d6c7ff85a909a
Djekson <alekssyvorov29@gmail.com>
2023-07-05 13:19:25 +0300

    1

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
-from random import randint
+def odd_numbers(a: int, b: int):
+    for i in range(a, b+1):
+        if i % 2 == 1:
+            print(i)
 
-def arr()->list:
-    lst = [randint(-10, 30) for i in range(randint(5, 15))]
-    return lst
 
-def task_1()->int:
-    summa = 0
-    lst = arr()
-    for i in lst:
-        if i > 0:
-            summa += i
-    return lst
+def main():
+    a = int(input('A '))
+    b = int(input('B '))
+    odd_numbers(a, b)
 
-def task_2():
-    lst = [randint(-30, 10) for i in range(30)]
-    summa = 0
-    for i in lst:
-        if i > 0:
-            break
-        else:
-            summa += i
-    return summa
 
-if __name__ == '__main__':
-    print(task_1())
-    print(task_2())
+if __name__ == "__main__":
+    main()
